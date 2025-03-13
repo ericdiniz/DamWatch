b33961e455b24c0c295c5e051911bc1077f5be32
Eric Diniz <64018893+ericdiniz@users.noreply.github.com>
2025-03-12 21:18:39 -0300

    new app module for backend

--- backend/src/app.module.js
+++ backend/src/app.module.js
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'sua_senha',
+      database: 'damwatch',
+      entities: [__dirname + '/**/*.entity{.js,.ts}'],
+      synchronize: true,
+      logging: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
